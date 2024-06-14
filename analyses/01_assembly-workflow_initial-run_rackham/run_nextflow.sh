6ce8673efca20c0869f9f66aef782897cbc20333
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2024-06-14 13:53:49 +0000

    modify nextflow run script to run invisibly on rackham or dardel

--- analyses/01_assembly-workflow_initial-run_rackham/run_nextflow.sh
+++ analyses/01_assembly-workflow_initial-run_rackham/run_nextflow.sh
@@ -2,37 +2,68 @@
 
 set -euo pipefail
 
-# NAISS compute allocation
-COMPUTEALLOC='/proj/snic2021-5-291'
-# NAISS storage allocation
-STORAGEALLOC='/proj/snic2021-6-194'
-# Nextflow work directory (e.g. /proj/snic2021-6-194/BGE-species/analyses/01_.../ -> /proj/snic2021-6-194/nobackup/BGE-species/analyses/01_.../nxf-work)
-WORKDIR="${PWD/$STORAGEALLOC/$STORAGEALLOC/nobackup}/nxf-work"
-# Path to store results from Nextflow
-RESULTS="${PWD/analyses/data/outputs}"
-# Path to Nextflow script. Pulls from Github
-NXF_SCRIPT="NBISweden/Earth-Biogenome-Project-pilot/main.nf"
-# Set common path to store all Singularity containers
-export NXF_SINGULARITY_CACHEDIR="${STORAGEALLOC}/nobackup/ebp-singularity-cache"
+function get_cluster_name {
+    if command -v sacctmgr >/dev/null 2>&1; then
+        # Only return cluster names we're catering for
+        sacctmgr show cluster -P -n \
+        | cut -f1 -d'|' \
+        | grep "rackham\|dardel"
+    fi
+}
 
-# Activate shared Nextflow environment
-eval "$(conda shell.bash hook)"
-conda activate "${STORAGEALLOC}/conda/nextflow-env"
+function run_nextflow {
+    PROFILE="$1"                                # Profile to use (values: uppmax, dardel)
+    STORAGEALLOC="$2"                           # NAISS storage allocation (path)
+    WORKDIR="${PWD/analyses/nobackup}/nxf-work" # Nextflow work directory
+    RESULTS="${PWD/analyses/data/outputs}"      # Path to store results from Nextflow
 
-# TODO: Clean results folder
+    # Path to Nextflow script. Pulls from Github
+    NXF_SCRIPT="NBISweden/Earth-Biogenome-Project-pilot/main.nf"
+    # Workflow version or branch to use (default: main)
+    NXF_BRANCH="${NXF_BRANCH:-main}"
 
-# Run Nextflow
-nextflow run "$NXF_SCRIPT" \
-    -r main \
-    -latest \
-    -profile uppmax,execution_report \
-    -work-dir "$WORKDIR" \
-    -resume \
-    -ansi-log false \
-    -params-file workflow_parameters.yml \
-    --cache "${STORAGEALLOC}/nobackup/database-cache" \
-    --outdir "$RESULTS"
+    # Set common path to store all Singularity containers
+    export NXF_SINGULARITY_CACHEDIR="${STORAGEALLOC}/nobackup/ebp-singularity-cache"
 
-# Clean up Nextflow cache to remove unused files
-nextflow clean -f -before $( nextflow log -q | tail -n 1 )
-# Use `nextflow log` to see the time and state of the last nextflow executions.
+    # Activate shared Nextflow environment
+    eval "$(conda shell.bash hook)"
+    conda activate "${STORAGEALLOC}/conda/nextflow-env"
+
+    # Clean results folder if last run resulted in error
+    if [ "$( nextflow log | awk -F $'\t' '{ last=$4 } END { print last }' )" == "ERR" ]; then
+        echo "WARN: Cleaning results folder due to previous error" >&2
+        rm -rf "$RESULTS"
+    fi
+
+    # Run Nextflow
+    nextflow run "$NXF_SCRIPT" \
+        -r "$NXF_BRANCH" \
+        -latest \
+        -profile "$PROFILE",execution_report \
+        -work-dir "$WORKDIR" \
+        -resume \
+        -ansi-log false \
+        -params-file workflow_parameters.yml \
+        --cache "${STORAGEALLOC}/nobackup/database-cache" \
+        --outdir "$RESULTS"
+
+    # Clean up Nextflow cache to remove unused files
+    nextflow clean -f -before "$( nextflow log -q | tail -n 1 )"
+    # Use `nextflow log` to see the time and state of the last nextflow executions.
+
+}
+
+# Detect cluster name ( rackham, dardel )
+cluster=$( get_cluster_name )
+
+# Run Nextflow with appropriate settings
+if [ "$cluster" == "rackham" ]; then
+    run_nextflow uppmax /proj/snic2021-6-194
+elif [ "$cluster" == "dardel" ]; then
+    run_nextflow dardel /cfs/klemming/projects/snic/snic2021-6-194
+else 
+    echo "Error: unrecognised cluster '$cluster'." >&2
+    exit 1
+fi
+
+
